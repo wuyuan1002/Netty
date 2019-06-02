4f3598e815d95566a5b50c8b21b616d4e323df59
wuyuan1002 <2461190767@qq.com>
2019-06-02 19:08:21 +0800

    第一个netty程序

--- src/main/java/com/wuyuan/netty/HttpServerHandler.java
+++ src/main/java/com/wuyuan/netty/HttpServerHandler.java
@@ -13,6 +13,8 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * 通道处理器
  *
@@ -23,14 +25,24 @@ import io.netty.util.CharsetUtil;
  * @date 2019/6/2
  */
 public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
 
         if (msg instanceof HttpRequest) {
-            System.err.println("请求来了");
+            System.out.println("----- 请求来了 -----");
+            HttpRequest httpRequest = (HttpRequest)msg;
+
+
+            System.out.println("请求方法名："+httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            if ("/wuyuan".equals(uri.getPath())){
+                System.out.println("请求访问了wuyuan");
+            }
+
 
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
-
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
 
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "test/plain");
@@ -38,6 +50,39 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
             ctx.writeAndFlush(response);
         }
-
     }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active --- 方法被调用");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered --- 方法被调用");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added --- 方法被调用");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inactive --- 方法被调用");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered --- 方法被调用");
+        super.channelUnregistered(ctx);
+    }
+
+
+
+
 }
