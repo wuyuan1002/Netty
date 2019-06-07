5ff58afa1eda9cda6d2d4ffb65282ceb0dd9e2e6
wuyuan1002 <2461190767@qq.com>
2019-06-07 16:16:46 +0800

    啊

--- src/main/java/com/wuyuan/nettyhttpdemo/HttpServer.java
+++ src/main/java/com/wuyuan/nettyhttpdemo/HttpServer.java
@@ -9,6 +9,8 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 /**
  * 服务端
  *
+ * 任意目录下打开cmd，输入curl "http://localhost:8088" 访问
+ *
  * @author wuyuan
  * @version 1.0
  * @date 2019/6/1
