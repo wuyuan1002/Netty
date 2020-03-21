fc9b7a928950a3b8a69d8a623fb298e15bf3fa0a
wuyuan1002 <2461190767@qq.com>
2020-03-21 09:58:49 +0800

    handler

--- src/main/java/com/wuyuan/httpdemo/HttpServer.java
+++ src/main/java/com/wuyuan/httpdemo/HttpServer.java
@@ -28,9 +28,13 @@ public class HttpServer {
         try {
             // 创建netty提供的用于启动服务器的类
             ServerBootstrap sbs = new ServerBootstrap();
-            // 传入事件循环组、连接通道的类型(之后通过反射创建channel对象)、日志处理器、channel初始化器
-            // 这里得handler和childHandler的区别是:
-            //      handler是针对于bossGroup的处理器，childHandler是针对于workerGroup的处理器
+            
+            /*
+             * 传入事件循环组、连接通道的类型(之后通过反射创建channel对象)、日志处理器、channel初始化器
+             *
+             * 这里的.handler()和.childHandler()的区别是:
+             *      .handler()是bossGroup上的处理器，而.childHandler()是workerGroup上的处理器
+             */
             sbs.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .handler(new LoggingHandler(LogLevel.INFO))
